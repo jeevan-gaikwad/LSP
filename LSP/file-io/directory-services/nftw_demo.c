1e9c1650efe2c2920049d9223c8d83c386a4891d
jeevan-gaikwad <jcgaikwad@gmail.com>
2017-01-23 20:27:32 +0530

    Added scandir and nftw client

---
+++ LSP/file-io/directory-services/nftw_demo.c
@@ -0,0 +1,93 @@
+#define _XOPEN_SOURCE 500
+#include<ftw.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<getopt.h>
+#include<errno.h>
+
+#define TRUE 1
+#define FALSE 0
+
+void print_usage(char *program_name);
+void print_version();
+int file_callback(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf);
+
+char recursive_flag=FALSE;
+
+int main(int argc, char *argv[]){
+	char *program_name=argv[0];
+	int optind=0;
+	int ret,flags=0;
+	char option_specified=FALSE;
+	char *directory_name=NULL;
+	
+	if(argc<2){
+		directory_name=".";
+	}
+	static struct option long_options[]={
+		{"version"  ,no_argument,NULL,'v'},
+		{"help"     ,no_argument,NULL,'h'},
+		{"recursive",no_argument,NULL,'r'},
+		{0          ,0          ,0   ,0  }
+	};
+	
+	while((ret=getopt_long(argc,argv,"hvr:",long_options,&optind))!=-1){
+		//option_specified=TRUE;
+		switch(ret){
+			case 'h':
+					print_usage(program_name);
+					exit(EXIT_SUCCESS);
+					break;
+			case 'v':
+					print_version();
+					exit(EXIT_SUCCESS);
+					break;
+			case 'r':
+					recursive_flag=TRUE;
+					directory_name=optarg;
+					break;
+			default:
+					fprintf(stderr,"Invalid option %c\n",ret);
+					print_usage(program_name);
+					exit(EXIT_FAILURE);
+		}
+	}
+
+	if(recursive_flag==FALSE){
+		directory_name=argv[1];
+	}
+		flags|=FTW_ACTIONRETVAL;
+	if(nftw(directory_name,file_callback,20,flags)==-1){
+		fprintf(stderr,"nftw failed. Error:%s",strerror(errno));
+	}
+
+	exit(EXIT_SUCCESS);
+}
+void print_usage(char *program_name){
+	printf("Usage: %s -h/--help :For options available with %s\n \
+			-v/--version :For program version info\n\
+			-r/--recursive :To display file list of all subdirectories\n\
+			<directory name> default is current directory\n", program_name,program_name); 
+	
+}
+
+void print_version(){
+	printf("v0.1\n");
+}
+
+int file_callback(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf){
+
+	if(typeflag==FTW_D && recursive_flag==TRUE){
+		printf("DIR %s\n",fpath);
+	}else if(recursive_flag==FALSE)
+		return FTW_SKIP_SUBTREE;
+	else
+		printf("%s\n",fpath);
+
+	return FTW_CONTINUE;
+}
+
+
+
+
+
---
+++ LSP/file-io/directory-services/scandir_demo.c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include<opt.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<errno.h>
+#include<string.h>
+#include<dirent.h>
+
+#define TRUE 1
+#define FALSE 0
+#define EQUAL 0
+#define NON_ZERO 1
+#define ZERO 0
+
+int  c_extn_filter(const struct dirent *dir_entry);
+int  file_name_length_comparator(const struct dirent ** dirent1,const struct dirent ** dirent2);
+void print_usage(char *program_name);
+void display_entries(const struct dirent **namelist,int no_of_entries);
+void cleanup(struct dirent **namelist,int no_of_entries);
+
+char *file_extension=NULL;
+
+int main(int argc, char* argv[])
+{
+	struct dirent **namelist=NULL;
+	int no_of_entries=0,i;
+
+	if(argc!=3){
+		print_usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	file_extension=argv[1];
+	char *directory_name=argv[2];
+
+	no_of_entries=get_directory_entries(directory_name,namelist);	
+
+	display_entries(namelist,no_of_entries);
+
+	//cleanup
+	cleanup(namelist,no_of_entries);	
+	exit(EXIT_SUCCESS);
+}
+int get_directory_entries(const char *directory_name,struct dirent **namelist){
+
+	int no_of_entries=scandir(directory_name,&namelist,c_extn_filter,file_name_length_comparator);
+
+	if(no_of_entries==-1){
+		fprintf(stderr,"failed to read %s for files with extenison %s\n",directory_name,file_extension);
+		exit(EXIT_FAILURE);
+	}
+	if(no_of_entries==0){
+		fprintf(stderr,"File(s) with extenison %s not found in the specified directory\n",file_extension);
+		exit(EXIT_FAILURE);
+	}
+	
+	return no_of_entries;
+}
+
+int c_extn_filter(const struct dirent *dir_entry){
+	
+	char const *file_name=dir_entry->d_name;
+	int file_extn_length=strlen(file_extension);
+	if(strncmp(file_extension,&file_name[strlen(file_name)-file_extn_length],file_extn_length)==EQUAL){
+		return NON_ZERO;
+	}else
+		return ZERO;
+}
+
+int file_name_length_comparator(const struct dirent ** dirent1,const struct dirent ** dirent2){
+
+	int f_name_len1=strlen((*dirent1)->d_name);
+	int f_name_len2=strlen((*dirent2)->d_name);
+
+	if(f_name_len1 > f_name_len2)
+		return 1;
+	else if(f_name_len1 < f_name_len2)
+		return -1;
+	else
+		return 0;
+
+}
+
+void print_usage(char *program_name)
+{
+	printf("Usage: %s <file extension> <directory name>\n",program_name);
+}
+
+void display_entries(const struct dirent **namelist,int no_of_entries){
+	int i;
+	for(i=0;i<no_of_entries;i++){
+		printf("%s\n",namelist[i]->d_name);
+	}
+}
+
+void cleanup(struct dirent **namelist,int no_of_entries){
+	int i;
+	for(i=0;i<no_of_entries;i++){
+		free(namelist[i]);
+	}
+	free(namelist);
+}
