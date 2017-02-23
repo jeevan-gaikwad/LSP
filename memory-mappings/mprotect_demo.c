dfa5eefe41e183b86db2a4f20b9e4c42761fbce3
jeevan-gaikwad <jcgaikwad@gmail.com>
2017-02-23 23:21:12 +0530

    Implemented using mmap

--- memory-mappings/mprotect_demo.c
+++ memory-mappings/mprotect_demo.c
@@ -5,20 +5,31 @@
 #include <sys/mman.h>
 
 #define PAGE_SIZE 4096
+
 typedef int (*sample_fn_t)(void);
 
 int main(int argc,char *argv[]){
-	char code[]={0x55,0x48,0x89,0xe5,0xb8,0x0a,0x00,0x00,0x00,0x5d,0xc3};		
-	char a='A';
-	char b='B';
-	char *ptr=&a;
+	char code[]={0x55,0x48,0x89,0xe5,0xb8,0x0d,0x00,0x00,0x00,0x5d,0xc3};		
 	sample_fn_t fn;
 	int ret=-1,error_code;
-	unsigned long int pg_base_addr=(unsigned long int)(code)- 0xFFFFFFFFFFFFF000;
-	printf("Ptr addrs:%p\n&b:%p \nPage base addr:%p code[1]:%p\n",ptr,&b,(void*)pg_base_addr,(void*)&code[1]);
+
+	void *mapped_addr=mmap(NULL,PAGE_SIZE,PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS,0,0);
+	if(mapped_addr==MAP_FAILED){
+		printf("mmap failed\n");
+		exit(EXIT_FAILURE);
+	}else
+		printf("mmap success\n");
+
+	void *ret_val_addr=memcpy(mapped_addr,(void*)code,sizeof(code));
+	if(ret_val_addr!=NULL){
+		printf("memcpy success!\n");
+	}else{
+		printf("mcpy failed! Cause:%s\n",strerror(error_code));
+		exit(EXIT_FAILURE);
+	}
 	
-	ret=mprotect((void *)code,PAGE_SIZE,PROT_EXEC|PROT_READ);
-	error_code=errno;
+	//on memcpy success, ret_val_addr and mapped_addr should be same
+	ret=mprotect(mapped_addr,PAGE_SIZE,PROT_EXEC|PROT_READ);	
 	if(ret==0){
 		printf("mprotect success!\n");
 	}else{
@@ -26,8 +37,9 @@ int main(int argc,char *argv[]){
 		exit(EXIT_FAILURE);
 	}
 	
-	fn=(sample_fn_t)code;
-	(*fn)();
+	fn=(sample_fn_t)mapped_addr;
+	printf("Function called with ret val:%d\n",(*fn)());
+
 	exit(EXIT_SUCCESS);
 }
 
